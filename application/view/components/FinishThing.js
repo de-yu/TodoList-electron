3885dc9828db57ce29784064415a0c9c329f42b0
de-yu <symbian3meego@Gmail.com>
2019-03-20 23:20:32 +0800

    finish

--- application/view/components/FinishThing.js
+++ application/view/components/FinishThing.js
@@ -1,20 +1,42 @@
 
 import React from 'react';
 import {Main} from './StyleComponent/Main'
+import {FinishBoard, Tab, TabMark, TabContent, ContentBlock, Date, ThingBlock, Thing} from './StyleComponent/FinishThing'
 
-export default class FinishThing extends React.Component
-{
-  constructor(props) {
-    super()
-  }
-  async componentDidMount  (){
-      
-  }
-  render() {
+export default class FinishThing extends React.Component{
+    constructor (props){
+        super()
+    }
+    async componentDidMount (){
+
+    }
+    render (){
         return(
-            <Main>
+                <Main>
+                    <FinishBoard>
+                        <Tab>
+                            <TabMark>
+                            2019
+                            </TabMark>
+                            <TabMark>
+                            2018
+                            </TabMark>
+                        </Tab>
+                        <TabContent>
+                            <ContentBlock>
+                                <Date>
+                                0318
+                                </Date>
+                                <ThingBlock>
+                                    <Thing>事項一</Thing>
+                                    <Thing>事項二</Thing>
+                                    
+                                </ThingBlock>
+                            </ContentBlock>
                 
-            </Main>
-            );
-  }
+                        </TabContent>
+                    </FinishBoard>
+                </Main>
+                );
+    }
 }
---
+++ application/view/components/StyleComponent/FinishThing.js
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+
+export const FinishBoard = styled.div
+`
+  position:relative;
+  top:20px;
+  background:#fff;
+  width:70%;
+  margin:0 auto;
+  margin-bottom:50px;
+  box-shadow:0px 1px 3px 1px #bbb; 
+`;
+
+export const Tab = styled.div
+`
+    height:60px;
+`;
+
+export const  TabMark = styled.span
+`
+    line-height:60px;
+`;
+
+export const TabContent = styled.div
+`
+
+`;
+
+export const ContentBlock = styled.div
+`
+
+`;
+
+
+export const Date = styled.div
+`
+      background:#aaa;
+      color:#fff;
+`;
+export const ThingBlock = styled.ul
+`
+`
+export const Thing = styled.li
+`
+`;
